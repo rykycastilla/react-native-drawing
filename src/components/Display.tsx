8527a2578ca46de7e4299704a9f4a45509213de9
Orestes Ricardo Castilla Escalona <rcastilla744@gmail.com>
2024-09-09 13:38:43 -0700

    adapt component: Display

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,41 +1,31 @@
+import CanvasDisplay from './CanvasDisplay'
 import { CoordinatesService, DrawingService } from '../modules/draw/services'
 import { Matrix } from '../modules/draw/models'
 import { ReactElement, useCallback, useEffect } from 'react'
-import { StyleSheet } from 'react-native'
 import { Tool } from '../modules/tools/models'
-import { TouchDetectedEvent, TouchEndEvent, TouchService } from '../modules/touch/services'
+import { TouchDetectedEvent, TouchEndEvent } from '../modules/touch/services'
 import { useCoordinatesService, useDisplay, useDrawingService, useMatrix } from '../modules/draw/hooks'
 
-import GLDisplay from './GLDisplay'
-
-export interface DisplayLayout {
-  top: number
-  left: number
-  x: number
-  y: number
-}
+import { useTouchPosition } from '../modules/touch/hooks'
 
 interface DisplayProps {
   resolution: number
-  touch: TouchService
-  layout: DisplayLayout
   tool: Tool
   onLoad( loaded:boolean ): void
 }
 
-const Display = ( props:DisplayProps ): ReactElement | null => {
+const Display = ( props:DisplayProps ): ReactElement => {
 
-  const { resolution, touch, layout, tool, onLoad } = props
-  const { x, y, size } = layout
+  const { resolution, tool, onLoad } = props
   const { display, loadDisplay } = useDisplay( resolution )
-  const matrix: Matrix = useMatrix( y, x, size, resolution, resolution, display )
+  const matrix: Matrix = useMatrix( resolution, resolution, display )
   const drawingService: DrawingService = useDrawingService( matrix )
-  const coordinatesService: CoordinatesService = useCoordinatesService( matrix )
+  const coordinatesService: CoordinatesService = useCoordinatesService( layoutRef )
+  const { touch, positionHandlers } = useTouchPosition()
 
   const onTouchDetected = useCallback( ( event:TouchDetectedEvent ) => {
-    const { targetId, x, y } = event
-    const { column, row } = coordinatesService.findCell( x, y )
-    drawingService.use( targetId, column, row, tool )
+    const { x, y } = coordinatesService.toInternal( event.x, event.y )
+    drawingService.use( event.targetId, x, y, tool )
   }, [ coordinatesService, drawingService, tool ] )
 
   const onTouchEnd = useCallback( ( event:TouchEndEvent ) => {
@@ -49,21 +39,12 @@ const Display = ( props:DisplayProps ): ReactElement | null => {
 
   return (
     <GLDisplay
-      style={ styles.display }
+      ref={ layoutRef }
+      positionHandlers={ positionHandlers }
       onLoad={ onLoad }
       onContextCreate={ loadDisplay } />
   )
 
 }
-
-const styles = StyleSheet.create( {
-
-  display: {
-    width: '100%',
-    height: '100%',
-    position : 'absolute',
-  },
-
-} )
 
 export default Display
