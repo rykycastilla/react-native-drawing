9759747f9c49a96e0def85103c18e35239fa49f0
Orestes Ricardo Castilla Escalona <rcastilla744@gmail.com>
2024-11-29 10:20:26 -0500

    feat: Supporting default env data

---
+++ scripts/create-env.js
@@ -0,0 +1,15 @@
+import { stringify } from '../libs/json-doc@1.0.0/index.js'
+import { writeFile } from 'node:fs/promises'
+
+const ENV_FILE = '.env.json'
+
+async function main() {
+  const envContent = stringify( defaultEnv )
+  await writeFile( ENV_FILE, envContent )
+}
+
+const defaultEnv = {
+  WEB_CORE_DEV_IP: '127.0.0.1',
+}
+
+main()
