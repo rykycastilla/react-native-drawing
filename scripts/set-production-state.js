6114e3ad2d93192a643043ec16347065fe2e8386
Orestes Ricardo Castilla Escalona <rcastilla744@gmail.com>
2024-11-30 13:29:23 -0500

    lint: reorganizing imports of set-producion-state

--- scripts/set-production-state.js
+++ scripts/set-production-state.js
@@ -1,8 +1,7 @@
+import { dirname, join } from 'node:path'
+import { fileURLToPath } from 'node:url'
 import { parse, stringify } from '../libs/json-doc@1.0.0/index.js'
 import { readFile, writeFile } from 'node:fs/promises'
-
-import { fileURLToPath } from 'node:url'
-import { dirname, join } from 'node:path'
 
 const __filename = fileURLToPath( import.meta.url )
 const __dirname = dirname( __filename )
